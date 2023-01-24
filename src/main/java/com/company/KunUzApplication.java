25aa3188f5f4ed44001af02eb551a6bebeb30830
Abdurasul <abdurasulismatullayev1997@gmail.com>
2023-01-24 16:47:44 +0500

    just changed

--- src/main/java/com/company/KunUzApplication.java
+++ src/main/java/com/company/KunUzApplication.java
@@ -10,6 +10,8 @@ public class KunUzApplication {
     public static void main(String[] args) {
         SpringApplication.run(KunUzApplication.class, args);
 
+        System.out.println();
+
 
     }
 
